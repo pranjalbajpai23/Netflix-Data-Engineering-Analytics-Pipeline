2eddee1f1120b952e83ef271a3050776da293a13
pranjalbajpai23 <pranjal.bajpai400@gmail.com>
2026-09-13 02:57:24 +0530

    added fact materialized as table

---
+++ netflix/models/fact/fact_genome_scores.sql
@@ -0,0 +1,9 @@
+with src_scores as (
+    select * from {{ref("src_genome_score")}}
+)
+select 
+    movie_id,
+    tag_id,
+    ROUND(relevance, 4) as relevance_score
+from src_scores
+where relevance>0
